45c921b30d615047d2bda11bc7b8017012032535
Vasili Raminashvili <vaska.r02@gmail.com>
2024-06-28 22:28:02 +0400

    news form

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,15 +5,15 @@ namespace App\Models;
 use App\Enums\NewsStatus;
 use Carbon\Carbon;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
@@ -51,30 +51,48 @@ class News extends Model
     public static function getForm(): array
     {
         return [
-            TextInput::make('title')
-                ->required()
-                ->maxLength(400),
-//            TextInput::make('slug')
-//                ->required()
-//                ->maxLength(255),
-            Textarea::make('short_description')
-                ->required()
-                ->columnSpanFull(),
-            Textarea::make('description')
-                ->required()
-                ->columnSpanFull(),
-            DateTimePicker::make('publish_date'),
-            Select::make('author_id')
-                ->relationship('author', 'name'),
-            TextInput::make('status')
-                ->required()
-                ->maxLength(255),
-            TextInput::make('views')
-                ->numeric(),
-            TextInput::make('sort')
-                ->numeric(),
-            TextInput::make('place')
-                ->maxLength(255),
+            Section::make()
+                ->columns(2)
+                ->schema([
+                    TextInput::make('title')
+                        ->required()
+                        ->maxLength(400),
+                    DateTimePicker::make('publish_date')
+                        ->seconds(false)
+                        ->default(now())
+                        ->native(false)
+                        ->minDate(now())
+                        ->firstDayOfWeek(1),
+                    TextInput::make('slug')
+                        ->hiddenOn('create')
+                        ->required()
+                        ->columnSpanFull(),
+                ]),
+            Section::make()
+                ->schema([
+                    RichEditor::make('description')
+                        ->required()
+                        ->columnSpanFull(),
+                    RichEditor::make('short_description')
+                        ->required()
+                        ->columnSpanFull(),
+
+                ]),
+            Section::make()
+                ->columns(2)
+                ->schema([
+                    Select::make('author_id')
+                        ->relationship('author', 'name')
+                        ->required(),
+                    TextInput::make('status')
+                        ->required()
+                        ->maxLength(255),
+                    TextInput::make('sort')
+                        ->numeric(),
+                    TextInput::make('place')
+                        ->maxLength(255),
+
+                ])
         ];
 
     }
